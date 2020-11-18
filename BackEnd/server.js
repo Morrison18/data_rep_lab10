5a3839671c9aa117780040d2c2609a0352d59122
Brian Morrison <brian@Brians-iMac.local>
2020-11-18 14:27:33 +0000

    lab 7 complete

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 4000
 const cors = require('cors');
 const bodyParser = require('body-parser')
-
+const mongoose =require('mongoose');
 
 // allows use to indicate a different domian and load its resources
 app.use(cors());
@@ -21,36 +21,55 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+// setting up connecting to the database server 
+const strConnection = 'mongodb+srv://brian:welcome20@cluster0.wvvof.mongodb.net/FilmsDB?retryWrites=true&w=majority';
+// connecting to database 
+mongoose.connect(strConnection, {useNewUrlParser: true});
+
+const Schema = mongoose.Schema;
+
+// creating an object for the schema 
+const movieSchema = new Schema({
+    Title:String,
+    Year:String,
+    Poster:String
+})
+// create object movieModel 
+const movieModel = mongoose.model('film', movieSchema);
+
+
 
 app.get('/api/movies', (req, res)=>{
-    // creating an array 
-    const mymovies = [
-        {
-            "Title": "Avengers: Infinity War",
-            "Year": "2018",
-            "imdbID": "tt4154756",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-            },
-            {
-            "Title": "Captain America: Civil War",
-            "Year": "2016",
-            "imdbID": "tt3498820",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-            }
-    ];
-    // status code 200 ok
-    res.status(200).json({
-        message: "All is good",
-        movies:mymovies
-    });
+    console.log(req.body);
+   
+    // going to database finding all docs and returning it in json format 
+    movieModel.find((err,data)=>{
+        res.json(data);
+    })
+   
 })
+// 
+app.get('/api/movies/:id', (req,res)=>{
 
+    console.log(req.params.id);
+
+    movieModel.findById(req.params.id, (err,data)=>{
+        res.json(data);
+    })
+})
 
 //when data passed up this takes the data and includes it into the array
 app.post('/api/movies', (req, res)=>{
-    console.log('Movie received ')
+    movieModel.create({
+        Title:req.body.Title,
+        Year:req.body.Year,
+        TPostertle:req.body.Poster
+
+    })
+    .then()
+    .catch();
+
+    res.send('Movie received ')
     console.log(req.body.Title);
     console.log(req.body.Year);
     console.log(req.body.Poster);
